e31fe5a63241cf1579371824072d66792e3786e3
wsingleton <wezley.singleton@gmail.com>
2021-11-07 17:37:26 -0500

    print welcome menu

---
+++ src/com/revature/quizzard/AppDriver.java
@@ -0,0 +1,18 @@
+package com.revature.quizzard;
+
+public class AppDriver {
+
+    public static void main(String[] args) {
+
+        String welcomeMenu = "Welcome to Quizzard!\n" +
+                             "Please make a selection from the options below:\n" +
+                             "1) Login\n" +
+                             "2) Register\n" +
+                             "3) Exit\n" +
+                             "> ";
+
+        System.out.println(welcomeMenu);
+
+    }
+
+}
