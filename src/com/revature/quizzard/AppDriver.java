a3b6f00a3922ad2fb0e8ad82d9912c5f5f4a5d09
wsingleton <wezley.singleton@gmail.com>
2021-11-07 18:08:06 -0500

    replaced recursive looping solution with a non-recursive one

--- src/com/revature/quizzard/AppDriver.java
+++ src/com/revature/quizzard/AppDriver.java
@@ -8,59 +8,65 @@ public class AppDriver {
 
     public static void main(String[] args) {
 
-        String welcomeMenu = "Welcome to Quizzard!\n" +
-                             "Please make a selection from the options below:\n" +
-                             "1) Login\n" +
-                             "2) Register\n" +
-                             "3) Exit\n" +
-                             "> ";
-
-        System.out.print(welcomeMenu);
-
         BufferedReader consoleReader = new BufferedReader(new InputStreamReader(System.in));
 
         String userSelection;
         try {
-             userSelection = consoleReader.readLine();
 
-             switch (userSelection) {
-                 case "1":
-                     System.out.println("You selected: Login.");
-                     System.out.println("Log into your account using your credentials");
-                     System.out.print("Username > ");
-                     String username = consoleReader.readLine();
-                     System.out.print("Password > ");
-                     String password = consoleReader.readLine();
-                     System.out.println("You entered: \nUsername - " + username + "\nPassword - " + password);
-                     break;
-                 case "2":
-                     System.out.println("You selected: Register");
-                     System.out.println("Please provide some information to register an account");
-                     System.out.print("First name > ");
-                     String firstName = consoleReader.readLine();
-                     System.out.print("Last name > ");
-                     String lastName = consoleReader.readLine();
-                     System.out.print("Email > ");
-                     String email = consoleReader.readLine();
-                     System.out.print("Username > ");
-                     String registerUsername = consoleReader.readLine();
-                     System.out.print("Password > ");
-                     String registerPassword = consoleReader.readLine();
+            boolean appRunning = true;
 
-                     System.out.printf("You entered: \n" +
-                                       "First name - %s\n" +
-                                       "Last name - %s\n" +
-                                       "Email - %s\n" +
-                                       "Username - %s\n" +
-                                       "Password - %s\n", firstName, lastName, email, registerUsername, registerPassword);
+            while (appRunning) {
 
-                     break;
-                 case "3":
-                     System.out.println("You selected: Exit");
-                     return;
-                 default:
-                     System.out.println("You have made an incorrect selection.");
-             }
+                String welcomeMenu = "Welcome to Quizzard!\n" +
+                        "Please make a selection from the options below:\n" +
+                        "1) Login\n" +
+                        "2) Register\n" +
+                        "3) Exit\n" +
+                        "> ";
+
+                System.out.print(welcomeMenu);
+
+                userSelection = consoleReader.readLine();
+
+                switch (userSelection) {
+                    case "1":
+                        System.out.println("You selected: Login.");
+                        System.out.println("Log into your account using your credentials");
+                        System.out.print("Username > ");
+                        String username = consoleReader.readLine();
+                        System.out.print("Password > ");
+                        String password = consoleReader.readLine();
+                        System.out.println("You entered: \nUsername - " + username + "\nPassword - " + password);
+                        break;
+                    case "2":
+                        System.out.println("You selected: Register");
+                        System.out.println("Please provide some information to register an account");
+                        System.out.print("First name > ");
+                        String firstName = consoleReader.readLine();
+                        System.out.print("Last name > ");
+                        String lastName = consoleReader.readLine();
+                        System.out.print("Email > ");
+                        String email = consoleReader.readLine();
+                        System.out.print("Username > ");
+                        String registerUsername = consoleReader.readLine();
+                        System.out.print("Password > ");
+                        String registerPassword = consoleReader.readLine();
+                        System.out.printf("You entered: \n" +
+                                "First name - %s\n" +
+                                "Last name - %s\n" +
+                                "Email - %s\n" +
+                                "Username - %s\n" +
+                                "Password - %s\n", firstName, lastName, email, registerUsername, registerPassword);
+                        break;
+                    case "3":
+                        System.out.println("You selected: Exit");
+                        appRunning = false;
+                        break;
+                    default:
+                        System.out.println("You have made an incorrect selection.");
+                }
+            }
+
         } catch (IOException e) {
             e.printStackTrace();
         } finally {
@@ -70,8 +76,6 @@ public class AppDriver {
                 e.printStackTrace();
             }
         }
-
-        main(args);
 
     }
 
