e2a8b2c0d2606b2111612435767bcd1632cca2d5
wsingleton <wezley.singleton@gmail.com>
2021-11-08 00:53:33 -0500

    basic login screen class added (no navigation yet)

---
+++ src/com/revature/quizzard/screens/LoginScreen.java
@@ -0,0 +1,53 @@
+package com.revature.quizzard.screens;
+
+import com.revature.quizzard.AppUser;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class LoginScreen extends Screen {
+
+    public LoginScreen() {
+        super("/login");
+    }
+
+    @Override
+    public void render() throws IOException {
+
+        BufferedReader consoleReader = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.println("\nLog into your account using your credentials");
+        System.out.print("Username > ");
+        String username = consoleReader.readLine();
+        System.out.print("Password > ");
+        String password = consoleReader.readLine();
+
+        if (username.trim().equals("") || password.trim().equals("")) {
+            System.err.println("You have provided invalid values. Navigating back to Welcome Screen...");
+            // TODO navigate back to WelcomeScreen
+            return;
+        }
+
+        System.out.println("[DEBUG] - You entered: \n\tUsername - " + username + "\n\tPassword - " + password);
+
+        BufferedReader dataReader = new BufferedReader(new FileReader("database/users.txt"));
+
+        String dataCursor;
+        while ((dataCursor = dataReader.readLine()) != null) {
+            String[] userData = dataCursor.split(":");
+            if (userData[3].equals(username) && userData[4].equals(password)) {
+                System.out.println("[DEBUG] - User found with matching credentials: " + dataCursor);
+                System.out.println("Credentials verified. Navigating to dashboard...");
+                AppUser authenticatedUser = new AppUser(userData[0], userData[1], userData[2], userData[3], userData[4]);
+                // TODO navigate to DashboardScreen
+                return;
+            }
+        }
+
+        System.err.println("No user found with matching credentials.");
+
+    }
+
+}
