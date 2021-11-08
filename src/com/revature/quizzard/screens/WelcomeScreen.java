0d45dab335f65fa9a8c8e8f3523747e5b113dd5f
wsingleton <wezley.singleton@gmail.com>
2021-11-08 00:46:44 -0500

    basic welcome screen class added (no functionality yet)

---
+++ src/com/revature/quizzard/screens/WelcomeScreen.java
@@ -0,0 +1,43 @@
+package com.revature.quizzard.screens;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class WelcomeScreen extends Screen {
+
+    public WelcomeScreen() {
+        super("/welcome");
+    }
+
+    @Override
+    public void render() throws IOException {
+
+        BufferedReader consoleReader = new BufferedReader(new InputStreamReader(System.in));
+
+        String welcomeMenu = "Welcome to Quizzard!\n" +
+                "Please make a selection from the options below:\n" +
+                "1) Login\n" +
+                "2) Register\n" +
+                "3) Exit\n" +
+                "> ";
+
+        System.out.print(welcomeMenu);
+
+        String userSelection = consoleReader.readLine();
+        switch (userSelection) {
+            case "1":
+                System.out.println("[DEBUG] - Login selected. Not implemented.");
+                break;
+            case "2":
+                System.out.println("[DEBUG] - Register selected. Not implemented.");
+                break;
+            case "3":
+                System.out.println("[DEBUG] - Exit selected. Not implemented.");
+                break;
+            default:
+                System.out.println("You have made an incorrect selection.");
+        }
+
+    }
+}
