cf102daee051dee381848cba9a4116e1c3d2a701
wsingleton <wezley.singleton@gmail.com>
2021-11-08 20:48:55 -0500

    set up UserService class, preparing for refactor

---
+++ src/com/revature/quizzard/services/UserService.java
@@ -0,0 +1,15 @@
+package com.revature.quizzard.services;
+
+import com.revature.quizzard.models.AppUser;
+
+public class UserService {
+
+    public AppUser registerNewUser(AppUser newUser) {
+        return null;
+    }
+
+    public AppUser authenticate(String username, String password) {
+        return null;
+    }
+
+}
