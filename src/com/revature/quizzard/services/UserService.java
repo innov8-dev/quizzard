f0f31c6e72b7fd20466eb417f4a286d982068f70
wsingleton <wezley.singleton@gmail.com>
2021-11-08 23:13:14 -0500

    added registration checks to prevent duplicate usernames and emails

--- src/com/revature/quizzard/services/UserService.java
+++ src/com/revature/quizzard/services/UserService.java
@@ -16,6 +16,15 @@ public class UserService {
             throw new InvalidRequestException("Invalid registration values provided!");
         }
 
+        boolean usernameTaken = !isUsernameAvailable(newUser.getUsername());
+        boolean emailTaken = !isEmailAvailable(newUser.getEmail());
+        if (usernameTaken || emailTaken) {
+            String msg = "The values provided for the following fields are already taken:";
+            if (usernameTaken) msg = msg + "\n\t- username";
+            if (emailTaken) msg = msg + "\n\t- email";
+            throw new ResourcePersistenceException(msg);
+        }
+
         System.out.println("[DEBUG] - User object provided: " + newUser);
 
         try {
@@ -44,6 +53,14 @@ public class UserService {
             throw new AuthenticationException(dse);
         }
 
+    }
+
+    public boolean isUsernameAvailable(String username) {
+        return (userRepo.findByUsername(username) == null);
+    }
+
+    public boolean isEmailAvailable(String email) {
+        return (userRepo.findByEmail(email) == null);
     }
 
 }
