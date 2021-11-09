035ce0189771d2a536dd3f06f89d42e0a669a16f
wsingleton <wezley.singleton@gmail.com>
2021-11-08 23:04:47 -0500

    fix incorrect constructor invocation

--- src/com/revature/quizzard/repositories/UserRepository.java
+++ src/com/revature/quizzard/repositories/UserRepository.java
@@ -1,7 +1,6 @@
 package com.revature.quizzard.repositories;
 
 import com.revature.quizzard.exceptions.DataSourceException;
-import com.revature.quizzard.exceptions.ResourceNotFoundException;
 import com.revature.quizzard.models.AppUser;
 import com.revature.quizzard.util.List;
 
@@ -56,7 +55,7 @@ public class UserRepository implements CrudRepository<AppUser, String> {
             while ((dataCursor = dataReader.readLine()) != null) {
                 String[] userData = dataCursor.split(":");
                 if (userData[4].equals(username) && userData[5].equals(password)) {
-                    return new AppUser(userData[0], userData[1], userData[2], userData[3], userData[4]);
+                    return new AppUser(userData[0], userData[1], userData[2], userData[3], userData[4], userData[5]);
                 }
             }
         } catch (IOException e) {
