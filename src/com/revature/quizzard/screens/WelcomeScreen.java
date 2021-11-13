b5b5bd9542aab34bef5d05c22060897103e6f25c
wsingleton <wezley.singleton@gmail.com>
2021-11-13 16:38:37 -0500

    added logs to WelcomeScreen

--- src/com/revature/quizzard/screens/WelcomeScreen.java
+++ src/com/revature/quizzard/screens/WelcomeScreen.java
@@ -3,11 +3,14 @@ package com.revature.quizzard.screens;
 import static com.revature.quizzard.util.AppContext.*;
 
 import com.revature.quizzard.util.ScreenRouter;
+import com.revature.quizzard.util.logging.Logger;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 
 public class WelcomeScreen extends Screen {
+
+    private static final Logger logger = Logger.getLogger();
 
     public WelcomeScreen(BufferedReader consoleReader, ScreenRouter router) {
         super("/welcome", consoleReader, router);
@@ -15,6 +18,7 @@ public class WelcomeScreen extends Screen {
 
     @Override
     public void render() throws IOException {
+        logger.info("Rendering WelcomeScreen");
         String welcomeMenu = "Welcome to Quizzard!\n" +
                 "Please make a selection from the options below:\n" +
                 "1) Login\n" +
@@ -37,6 +41,7 @@ public class WelcomeScreen extends Screen {
                 shutdown();
                 break;
             default:
+                logger.info("User made invalid selection on WelcomeScreen");
                 System.out.println("You have made an incorrect selection.");
         }
     }
