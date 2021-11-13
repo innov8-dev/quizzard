6edf858f40bfbb08b4a026d037666c6136b8efc1
wsingleton <wezley.singleton@gmail.com>
2021-11-13 16:42:38 -0500

    added logs to DashboardScreen

--- src/com/revature/quizzard/screens/DashboardScreen.java
+++ src/com/revature/quizzard/screens/DashboardScreen.java
@@ -2,11 +2,14 @@ package com.revature.quizzard.screens;
 
 import com.revature.quizzard.services.UserService;
 import com.revature.quizzard.util.ScreenRouter;
+import com.revature.quizzard.util.logging.Logger;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 
 public class DashboardScreen extends Screen {
+
+    private static final Logger logger = Logger.getLogger();
 
     private final UserService userService;
 
@@ -17,6 +20,8 @@ public class DashboardScreen extends Screen {
 
     @Override
     public void render() throws IOException {
+
+        logger.info("Rendering DashboardScreen");
 
         while (userService.isSessionActive()) {
             System.out.print("Welcome, " + userService.getSessionUser().getFirstName() + "!\n" +
@@ -30,16 +35,20 @@ public class DashboardScreen extends Screen {
 
             switch (userSelection) {
                 case "1":
+                    logger.info("User made selection to: Create a flashcard");
                     router.navigate("/new-flashcard");
                     break;
                 case "2":
+                    logger.info("User made selection to: View my flashcards");
                     router.navigate("/my-flashcards");
                     break;
                 case "3":
+                    logger.info("User made selection to: Logout");
                     System.out.println("Logging out and navigating back to Welcome Screen.");
                     userService.logout();
                     break;
                 default:
+                    logger.info("User made invalid selection on DashboardScreen");
                     System.out.println("You have made an incorrect selection.");
             }
         }
