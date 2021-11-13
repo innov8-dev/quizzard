a44ba7ab98c4bcf349f5c24253a48118fb5e859a
wsingleton <wezley.singleton@gmail.com>
2021-11-13 16:40:21 -0500

    added logs to LoginScreen

--- src/com/revature/quizzard/screens/LoginScreen.java
+++ src/com/revature/quizzard/screens/LoginScreen.java
@@ -4,11 +4,14 @@ import com.revature.quizzard.exceptions.AuthenticationException;
 import com.revature.quizzard.models.AppUser;
 import com.revature.quizzard.services.UserService;
 import com.revature.quizzard.util.ScreenRouter;
+import com.revature.quizzard.util.logging.Logger;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 
 public class LoginScreen extends Screen {
+
+    private static final Logger logger = Logger.getLogger();
 
     private final UserService userService;
 
@@ -19,6 +22,8 @@ public class LoginScreen extends Screen {
 
     @Override
     public void render() throws IOException {
+
+        logger.info("Rendering LoginScreen");
 
         System.out.println("\nLog into your account using your credentials");
         System.out.print("Username > ");
