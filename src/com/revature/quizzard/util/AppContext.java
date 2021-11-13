cbeab21d9910362792b0bc8aa700256f07d6cfe3
wsingleton <wezley.singleton@gmail.com>
2021-11-12 22:47:10 -0500

    included simple log statements in AppContext logic; TODO: improve logger so that log entries can have 'levels' and abstract timekeeping from the caller

--- src/com/revature/quizzard/util/AppContext.java
+++ src/com/revature/quizzard/util/AppContext.java
@@ -2,28 +2,38 @@ package com.revature.quizzard.util;
 
 import com.revature.quizzard.screens.*;
 import com.revature.quizzard.services.UserService;
+import com.revature.quizzard.util.logging.Logger;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
 public class AppContext {
 
+    private static final Logger logger = Logger.getLogger(true);
     private static boolean appRunning;
     private final ScreenRouter router;
 
     public AppContext() {
+
+        logger.log("Application initialization started at %s", System.currentTimeMillis());
+
         appRunning = true;
         BufferedReader console = new BufferedReader(new InputStreamReader(System.in));
         router = new ScreenRouter();
 
         UserService userService = new UserService();
 
+        logger.log("ScreenRouter population started at %s", System.currentTimeMillis());
         router.addScreen(new WelcomeScreen(console, router));
         router.addScreen(new RegisterScreen(console, router, userService));
         router.addScreen(new LoginScreen(console, router, userService));
         router.addScreen(new DashboardScreen(console, router, userService));
         router.addScreen(new ViewMyFlashcardsScreen(console, router, userService));
         router.addScreen(new CreateNewFlashcardScreen(console, router, userService));
+        logger.log("ScreenRouter population completed at %s", System.currentTimeMillis());
+
+        logger.log("Application initialization completed at %s", System.currentTimeMillis());
+
     }
 
     public void startup() {
@@ -32,14 +42,15 @@ public class AppContext {
                 router.navigate("/welcome");
             }
         } catch (Throwable t) {
-            t.printStackTrace();
+            logger.log("An unhandled exception of type %s occurred with the message: %s", t.getClass().getSimpleName(), t.getMessage());
             System.out.println("An unhandled exception occurred and the application must terminate now.");
         } finally {
-            appRunning = false;
+            shutdown();
         }
     }
 
     public static void shutdown() {
+        logger.log("Application shutdown request received at %s", System.currentTimeMillis());
         appRunning = false;
     }
 
