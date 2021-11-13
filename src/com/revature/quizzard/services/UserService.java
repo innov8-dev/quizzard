0c6e8f7842d03fc5aee6ebc2f3560a0ca7b4a508
wsingleton <wezley.singleton@gmail.com>
2021-11-13 16:35:47 -0500

    added logs to UserService

--- src/com/revature/quizzard/services/UserService.java
+++ src/com/revature/quizzard/services/UserService.java
@@ -3,34 +3,41 @@ package com.revature.quizzard.services;
 import com.revature.quizzard.exceptions.*;
 import com.revature.quizzard.models.AppUser;
 import com.revature.quizzard.repositories.UserRepository;
+import com.revature.quizzard.util.logging.Logger;
 
 public class UserService {
+
+    private static final Logger logger = Logger.getLogger();
 
     private AppUser sessionUser;
     private final UserRepository userRepo = new UserRepository();
 
     public AppUser registerNewUser(AppUser newUser) {
 
+        logger.info("User object provided: " + newUser);
+
         if (newUser.getFirstName().trim().equals("") || newUser.getLastName().trim().equals("") || newUser.getEmail().trim().equals("") ||
                 newUser.getUsername().trim().equals("") || newUser.getPassword().trim().equals(""))
         {
-            throw new InvalidRequestException("Invalid registration values provided!");
+            String msg = "Invalid registration values provided";
+            logger.warn(msg);
+            throw new InvalidRequestException(msg);
         }
 
         boolean usernameTaken = !isUsernameAvailable(newUser.getUsername());
         boolean emailTaken = !isEmailAvailable(newUser.getEmail());
         if (usernameTaken || emailTaken) {
-            String msg = "The values provided for the following fields are already taken:";
-            if (usernameTaken) msg = msg + "\n\t- username";
-            if (emailTaken) msg = msg + "\n\t- email";
+            String msg = "The values provided for the following fields are already taken: ";
+            if (usernameTaken) msg = msg + "username ";
+            if (emailTaken) msg = msg + "email";
+            logger.warn(msg);
             throw new ResourcePersistenceException(msg);
         }
-
-        System.out.println("[DEBUG] - User object provided: " + newUser);
 
         try {
             return userRepo.save(newUser);
         } catch (DataSourceException dse) {
+            logger.warn(dse.getMessage());
             throw new ResourcePersistenceException(dse);
         }
 
@@ -39,41 +46,50 @@ public class UserService {
     public AppUser authenticate(String username, String password) {
 
         if (username.trim().equals("") || password.trim().equals("")) {
-            throw new InvalidRequestException("Invalid credential values provided!");
+            String msg = "Invalid credential values provided";
+            logger.warn(msg);
+            throw new InvalidRequestException(msg);
         }
 
-        System.out.println("[DEBUG] - Credentials provided: {username=" + username + ", password=" + password + "}");
+        logger.info("Credentials provided: {username=%s, password=%s}", username, password);
 
         try {
             AppUser authenticatedUser = userRepo.findByUsernameAndPassword(username, password);
             if (authenticatedUser == null) {
+                logger.info("Could not authenticate using provided credentials");
                 throw new AuthenticationException();
             }
             sessionUser = authenticatedUser;
             return authenticatedUser;
         } catch (DataSourceException dse) {
+            logger.warn(dse.getMessage());
             throw new AuthenticationException(dse);
         }
 
     }
 
     public boolean isUsernameAvailable(String username) {
+        logger.info("Checking availability of the username: %s", username);
         return (userRepo.findByUsername(username) == null);
     }
 
     public boolean isEmailAvailable(String email) {
+        logger.info("Checking availability of the email address: %s", email);
         return (userRepo.findByEmail(email) == null);
     }
 
     public AppUser getSessionUser() {
+        logger.info("Fetching session user");
         return sessionUser;
     }
 
     public boolean isSessionActive() {
+        logger.info("Checking if session is active");
         return sessionUser != null;
     }
 
     public void logout() {
+        logger.info("Terminating current user session");
         sessionUser = null;
     }
 }
