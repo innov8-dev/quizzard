4ee371a0f3039c87674bc16535fee554110cd905
wsingleton <wezley.singleton@gmail.com>
2021-11-12 21:43:28 -0500

    AppContext base impl finished, still WIP (some screens are currently broken)

--- src/com/revature/quizzard/AppDriver.java
+++ src/com/revature/quizzard/AppDriver.java
@@ -1,9 +1,12 @@
 package com.revature.quizzard;
 
+import com.revature.quizzard.util.AppContext;
+
 public class AppDriver {
 
     public static void main(String[] args) {
-
+        AppContext app = new AppContext();
+        app.startup();
     }
 
 }
--- src/com/revature/quizzard/util/AppContext.java
+++ src/com/revature/quizzard/util/AppContext.java
@@ -1,15 +1,38 @@
 package com.revature.quizzard.util;
 
+import com.revature.quizzard.screens.*;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 public class AppContext {
 
     private static boolean appRunning;
+    private final ScreenRouter router;
 
     public AppContext() {
         appRunning = true;
+        BufferedReader console = new BufferedReader(new InputStreamReader(System.in));
+        router = new ScreenRouter();
+        router.addScreen(new WelcomeScreen(console, router));
+        router.addScreen(new RegisterScreen(console, router));
+        router.addScreen(new LoginScreen(console, router));
+        router.addScreen(new DashboardScreen(console, router, null)); // TODO refactor to not require third param
+        router.addScreen(new ViewMyFlashcardsScreen(console, router, null)); // TODO refactor to not require third param
+        router.addScreen(new CreateNewFlashcardScreen(console, router, null)); // TODO refactor to not require third param
     }
 
-    public static void startup() {
-
+    public void startup() {
+        try {
+            while (appRunning) {
+                router.navigate("/welcome");
+            }
+        } catch (Throwable t) {
+            t.printStackTrace();
+            System.out.println("An unhandled exception occurred and the application must terminate now.");
+        } finally {
+            appRunning = false;
+        }
     }
 
     public static void shutdown() {
